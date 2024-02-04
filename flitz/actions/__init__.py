6900585656ce1f006309469bb7dfd6cc90f500ad
Martin Thoma <info@martin-thoma.de>
2024-02-04 12:07:41 +0100

    MAINT: Move the ShowProperties logic into a MixIn

--- flitz/actions/__init__.py
+++ flitz/actions/__init__.py
@@ -5,5 +5,6 @@ Every MixIn should have its own file and be self-contained.
 """
 
 from flitz.actions.deletion import DeletionMixIn
+from flitz.actions.show_properties import ShowProperties
 
-__all__ = ["DeletionMixIn"]
+__all__ = ["DeletionMixIn", "ShowProperties"]
--- flitz/file_explorer.py
+++ flitz/file_explorer.py
@@ -4,7 +4,6 @@ import logging
 import shutil
 import sys
 import tkinter as tk
-from collections.abc import Iterable
 from datetime import datetime
 from pathlib import Path
 from tkinter import messagebox, ttk
@@ -12,7 +11,7 @@ from tkinter.simpledialog import askstring
 
 from PIL import Image, ImageTk
 
-from .actions import DeletionMixIn
+from .actions import DeletionMixIn, ShowProperties
 from .config import CONFIG_PATH, Config, create_settings
 from .context_menu import ContextMenuItem, create_context_menu
 from .ui_utils import ask_for_new_name
@@ -24,7 +23,7 @@ MIN_FONT_SIZE = 4
 MAX_FONT_SIZE = 40
 
 
-class FileExplorer(tk.Tk, DeletionMixIn):
+class FileExplorer(tk.Tk, DeletionMixIn, ShowProperties):
     """
     FileExplorer is an app for navigating and exploring files and directories.
 
@@ -177,91 +176,6 @@ class FileExplorer(tk.Tk, DeletionMixIn):
                 self.load_files(new_file_path)
             except OSError as e:
                 messagebox.showerror("Error", f"Failed to create file: {e}")
-
-    def show_properties(self) -> None:
-        """Show properties."""
-        selected_item = self.tree.selection()
-        if not selected_item:
-            return
-        if not isinstance(selected_item, tuple):
-            from .file_properties_dialog import FilePropertiesDialog
-
-            selected_file = self.tree.item(selected_item, "values")[
-                FileExplorer.NAME_INDEX
-            ]
-            file_path = self.current_path / selected_file
-            try:
-                file_stat = file_path.stat()
-                size = file_stat.st_size if file_path.is_file() else ""
-                type_ = "File" if file_path.is_file() else "Folder"
-                date_modified = datetime.fromtimestamp(file_stat.st_mtime).strftime(
-                    "%Y-%m-%d %H:%M:%S",
-                )
-
-                # Create and display the properties dialog form
-                properties_dialog = FilePropertiesDialog(
-                    file_name=selected_file,
-                    file_size=size,
-                    file_type=type_,
-                    date_modified=date_modified,
-                )
-                properties_dialog.focus_set()
-                properties_dialog.grab_set()
-                properties_dialog.wait_window()
-
-            except OSError as e:
-                messagebox.showerror("Error", f"Failed to retrieve properties: {e}")
-        else:
-            self._show_properties_file_selection_list(selected_item)
-
-    def _show_properties_file_selection_list(
-        self,
-        selected_item: Iterable[str],
-    ) -> None:
-        from .file_properties_dialog import FilePropertiesDialog
-
-        nb_files = 0
-        nb_folders = 0
-        size_sum = 0
-        date_modified_min = None
-        date_modified_max = None
-        for item in selected_item:
-            values = self.tree.item(item, "values")
-            selected_file = values[FileExplorer.NAME_INDEX]
-            file_path = self.current_path / selected_file
-            if file_path.is_file():
-                nb_files += 1
-            else:
-                nb_folders += 1
-            try:
-                file_stat = file_path.stat()
-                size_sum += file_stat.st_size if file_path.is_file() else 0
-                date_modified = datetime.fromtimestamp(file_stat.st_mtime)
-                if date_modified_min is None:
-                    date_modified_min = date_modified
-                else:
-                    date_modified_min = min(date_modified, date_modified_min)
-
-                if date_modified_max is None:
-                    date_modified_max = date_modified
-                else:
-                    date_modified_max = max(date_modified, date_modified_max)
-
-            except OSError as e:
-                messagebox.showerror("Error", f"Failed to retrieve properties: {e}")
-        # Create and display the properties dialog form
-        properties_dialog = FilePropertiesDialog(
-            file_name="",
-            file_size=size_sum,
-            file_type=f"({nb_files} files, {nb_folders} folders)",
-            date_modified=(
-                f"{date_modified_min:%Y-%m-%d %H:%M} - "
-                f"{date_modified_max:%Y-%m-%d %H:%M}"
-            ),
-        )
-        properties_dialog.focus_set()
-        properties_dialog.grab_set()
-        properties_dialog.wait_window()
 
     def exit_search_mode(self, _: tk.Event) -> None:
         """Exit the search mode."""
